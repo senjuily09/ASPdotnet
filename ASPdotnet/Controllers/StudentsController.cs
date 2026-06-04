856f6d5d9e95abdfe92b7d9f436f273de5b9a498
senjuily09 <gvsharshith14@gmail.com>
2026-06-04 11:22:45 +0530

    controllers

---
+++ ASPdotnet/Controllers/StudentsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPdotnot.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            return Ok("Hello Students");
+        }
+    }
+}
--- ASPdotnet/Program.cs
+++ ASPdotnet/Program.cs
@@ -2,28 +2,17 @@ using Microsoft.AspNetCore.Builder;
 
 namespace ASPdotnot
 {
-    public class Person
-    {
-        public string Name { get; set; }
-        public int Age { get; set; }
-    }
-
     public class Program
     {
         public static void Main(string[] args)
         {
-            var people = new List<Person>
-            {
-                new Person { Name = "Alice", Age = 30 }
-            };
-
-            foreach (var person in people)
-            {
-                Console.WriteLine($"{person.Name} {person.Age}");
-            }
-
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+
             var app = builder.Build();
+
+            app.MapControllers();
 
             app.MapGet("/", () => "Hello World!");
 
