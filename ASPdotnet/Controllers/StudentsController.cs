6abaf2528b07e2df4d27c43b161dc7a4c526de62
senjuily09 <gvsharshith14@gmail.com>
2026-06-04 15:50:29 +0530

    models

--- ASPdotnet/Controllers/StudentsController.cs
+++ ASPdotnet/Controllers/StudentsController.cs
@@ -9,22 +9,50 @@ namespace ASPdotnet.Controllers
         [HttpGet]
         public IActionResult GetStudents()
         {
-            return Ok("Hello Students");
+            return Ok("Changed");
         }
+
         [HttpPost]
         public IActionResult AddStudent()
         {
             return Ok("Student Added");
         }
+
         [HttpPut]
         public IActionResult UpdateStudent()
         {
             return Ok("Student Updated");
         }
+
         [HttpDelete]
         public IActionResult DeleteStudent()
         {
             return Ok("Student Deleted");
+        }
+
+        [HttpGet("all")]
+        public IActionResult GetAllStudents()
+        {
+            var students = new List<ASPdotnet.Models.Student>
+            {
+                new ASPdotnet.Models.Student
+                {
+                    Id = 1,
+                    Name = "Alice",
+                    Age = 20,
+                    Department = "CSE"
+                },
+
+                new ASPdotnet.Models.Student
+                {
+                    Id = 2,
+                    Name = "Bob",
+                    Age = 22,
+                    Department = "ECE"
+                }
+            };
+
+            return Ok(students);
         }
     }
 }
