5229b2823e4c6f8d0e2f27d3ff5ffd5a7d3578b6
senjuily09 <gvsharshith14@gmail.com>
2026-06-05 14:51:06 +0530

    swagger

--- ASPdotnet/Controllers/PracticeController.cs
+++ ASPdotnet/Controllers/PracticeController.cs
@@ -12,5 +12,15 @@ namespace ASPdotnet.Controllers
         {
             return Ok(student);
         }
+        [HttpPut("{id}")]
+        public IActionResult UpdateStudent(int id, Student student)
+        {
+            return Ok($"Student {id} updated to {student.Name}");
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            return Ok($"Student {id} deleted");
+        }
     }
-}
+} 
