c2576b2bcff2fdd4d0d7a841e9aed7aa69551711
senjuily09 <gvsharshith14@gmail.com>
2026-06-05 12:42:59 +0530

    POST and swagger

---
+++ ASPdotnet/Controllers/PracticeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ASPdotnet.Models;
+
+namespace ASPdotnet.Controllers
+{
+    [ApiController]
+    [Route("api/practice")]
+    public class PracticeController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult AddStudent(Student student)
+        {
+            return Ok(student);
+        }
+    }
+}
--- ASPdotnet/Program.cs
+++ ASPdotnet/Program.cs
@@ -4,15 +4,30 @@ namespace ASPdotnet
 {
     public class Program
     {
-        public static void Main(String[] args)
+        public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddControllers();
-            var app = builder.Build();
-            app.MapControllers();
-            app.MapGet("/", () => "hi");
-            app.Run();
 
+            // Enable Controllers
+            builder.Services.AddControllers();
+
+            // Enable Swagger
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Enable Swagger UI
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            // Enable Controller Routes
+            app.MapControllers();
+
+            // Home Page
+            app.MapGet("/", () => "hi");
+
+            app.Run();
         }
     }
 }
