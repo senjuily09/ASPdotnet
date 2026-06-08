db2b816a1464bd5d1b9047ebe348460119cba5f3
senjuily09 <gvsharshith14@gmail.com>
2026-06-08 15:28:19 +0530

    database and ef core

--- ASPdotnet/Program.cs
+++ ASPdotnet/Program.cs
@@ -1,19 +1,42 @@
 using Microsoft.AspNetCore.Builder;
+using ASPdotnet.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace ASPdotnet
 {
     public class Program
     {
-        public static void Main(String[] args)
+        public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            // Enable Controllers
             builder.Services.AddControllers();
+
+
+            builder.Services.AddDbContext<AppDbContext>(
+            options =>
+        options.UseSqlServer(
+            builder.Configuration.GetConnectionString("DefaultConnection")
+        )
+);
+
+            // Enable Swagger
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
             var app = builder.Build();
+
+            // Enable Swagger UI
             app.UseSwagger();
             app.UseSwaggerUI();
-            app.MapGet("/", () => "hjk");
+
+            // Enable Controller Routes
             app.MapControllers();
+
+            // Home Page
+            app.MapGet("/", () => "hi");
+
             app.Run();
         }
     }
