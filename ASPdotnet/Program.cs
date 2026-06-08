2a9c71c512b52277337499837c7085d0469f7e82
senjuily09 <gvsharshith14@gmail.com>
2026-06-08 13:49:05 +0530

    not done yet

--- ASPdotnet/Program.cs
+++ ASPdotnet/Program.cs
@@ -1,32 +1,19 @@
 using Microsoft.AspNetCore.Builder;
-
 namespace ASPdotnet
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main(String[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-            // Enable Controllers
             builder.Services.AddControllers();
-
-            // Enable Swagger
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
             var app = builder.Build();
-
-            // Enable Swagger UI
             app.UseSwagger();
             app.UseSwaggerUI();
-
-            // Enable Controller Routes
+            app.MapGet("/", () => "hjk");
             app.MapControllers();
-
-            // Home Page
-            app.MapGet("/", () => "hi");
-
             app.Run();
         }
     }
